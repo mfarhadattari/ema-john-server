de3f1bb25d4bd35043fde839bfb45d359bf81209
mfarhadattari <mfarhad.attari@gmail.com>
2023-05-16 23:42:31 +0600

    pool create

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    maxPoolSize: 10,
   },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
 });
 
 async function run() {
